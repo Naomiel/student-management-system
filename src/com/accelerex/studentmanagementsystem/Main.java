09e72304556e677b91fd4468ace1bb47ee73a7f6
Naomi Unuakpa <naomi.unuakpa@globalaccelerex.com>
2023-09-29 10:05:03 +0100

    My First Task

--- src/com/accelerex/studentmanagementsystem/Main.java
+++ src/com/accelerex/studentmanagementsystem/Main.java
@@ -1,7 +1,25 @@
 package com.accelerex.studentmanagementsystem;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+   public static void main(String[] args) {
+      // Create students
+      Student student1 = new Student(1, "Alice", 20, "123 Main St");
+      Student student2 = new Student(2, "Bob", 22, "456 Elm St");
+
+      // Create courses
+      Course course1 = new Course("CS101", "Intro to Programming", "Prof. Smith", "Mon/Wed 9:00 AM");
+      Course course2 = new Course("MATH201", "Calculus I", "Prof. Johnson", "Tue/Thu 10:30 AM");
+
+
+      // Enroll students in courses
+      student1.enrollInCourse(course1);
+      student2.enrollInCourse(course1);
+      student2.enrollInCourse(course2);
+
+      // Display student and course information
+      student1.displayInfo();
+      System.out.println();
+      student2.displayInfo();
+   }
 }
